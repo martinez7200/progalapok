54d1ea12ff825359a9621ab71763600e1241b266
mi <90605588+martinez7200@users.noreply.github.com>
2021-10-20 14:21:07 +0200

    Add files via upload

---
+++ H&M.py
@@ -0,0 +1,9 @@
+a = int(input('Adj meg egy számmot! '))
+b = int(input('Adj meg egy másik számot! '))
+print('A két szám összege',a+b)
+print('A két szám különbsége',a-b)
+print('A két szám szorzata',a*b)
+print('A két szám hányadosa',a/b)
+
+
+
